c0eb36cb35f2b9f0a8a3eca88c22e00152c2f163
Liberbell <liberbell@gmail.com>
2024-03-30 15:13:48 +0900

    investment_calculator4

--- lesson2/investment_calculator.go
+++ lesson2/investment_calculator.go
@@ -1,10 +1,12 @@
 package main
 
+import "math"
+
 func main() {
 	var investmentAmount = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
 
-	var futureValue = investmentAmount * (1 + int(expectedReturnRate)/100)
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
 
 }
