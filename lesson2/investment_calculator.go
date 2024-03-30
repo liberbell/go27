e386928c733888797dc95b384408a092bee7f839
Liberbell <liberbell@gmail.com>
2024-03-30 15:28:36 +0900

    investment_calculator7

--- lesson2/investment_calculator.go
+++ lesson2/investment_calculator.go
@@ -11,7 +11,7 @@ func main() {
 	var expectedReturnRate = 5.5
 	years := 10.0
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
 	fmt.Println(futureRealValue)
