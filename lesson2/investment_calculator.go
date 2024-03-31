194b7f17f4f40d586faee00dd406cffb1884bee9
Liberbell <liberbell@gmail.com>
2024-04-01 08:34:33 +0900

    investment_calculator2

--- lesson2/investment_calculator.go
+++ lesson2/investment_calculator.go
@@ -22,6 +22,8 @@ func main() {
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Printf("Future Value: %v", futureValue)
 	fmt.Println("Future Value: ", futureValue)
 	fmt.Println("Future Value(adjusted for Inflation): ", futureRealValue)
 }
