3db9261667e9bec4453f17940741ca29dce4072a
Liberbell <liberbell@gmail.com>
2024-03-31 15:02:06 +0900

    profit_calculator3

--- prac1/profit_calculator.go
+++ prac1/profit_calculator.go
@@ -1,7 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	var revenue int
 	var expences int
 	var taxRate float64
+
+	fmt.Print("Input Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Input Expence: ")
+	fmt.Scan(&expences)
+
+	fmt.Print("Input Tax Rate: ")
+	fmt.Scan(&taxRate)
 }
