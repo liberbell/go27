a913b1507b2dd41fcb4ca5299df186a7cc631241
Liberbell <liberbell@gmail.com>
2024-04-02 08:31:50 +0900

    investment_calculator3

--- lesson2/investment_calculator.go
+++ lesson2/investment_calculator.go
@@ -37,8 +37,8 @@ func outputText(text string) {
 	fmt.Print(text)
 }
 
-func calculateFunction(investmentAmount, expectedReturnRate, years float64) float64 {
+func calculateFunction(investmentAmount, expectedReturnRate, years, inflationRate float64) (float64, float64) {
 	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	rfv := futureValue / math.Pow(1+inflationRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
 	return fv, rfv
 }
