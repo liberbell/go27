43d4dfc2743395fa1cf77f9493ee04dead3529fa
Liberbell <liberbell@gmail.com>
2024-04-02 08:50:54 +0900

    profit_calculator1

--- prac1/profit_calculator.go
+++ prac1/profit_calculator.go
@@ -4,23 +4,29 @@ import "fmt"
 
 func main() {
 	var revenue float64
-	var expences float64
+	var expenses float64
 	var taxRate float64
 
 	fmt.Print("Input Revenue: ")
 	fmt.Scan(&revenue)
 
 	fmt.Print("Input Expence: ")
-	fmt.Scan(&expences)
+	fmt.Scan(&expenses)
 
 	fmt.Print("Input Tax Rate: ")
 	fmt.Scan(&taxRate)
 
-	ebt := revenue - expences
+	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
+}
+
+func getUserInput(infoText string) {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
 }
