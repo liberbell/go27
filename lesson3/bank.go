de8afd4f5efc8d5982105cf94c49a86e2244e9bf
Liberbell <liberbell@gmail.com>
2024-04-04 09:03:45 +0900

    bank11

--- lesson3/bank.go
+++ lesson3/bank.go
@@ -8,49 +8,47 @@ func main() {
 	var withdrawAmount float64
 
 	for i := 0; i < 2; i++ {
+		fmt.Println("Welcome to Go Bank.")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1 Check balance")
+		fmt.Println("2 Deposit money")
+		fmt.Println("3 Withdraw money")
+		fmt.Println("4 Exit")
 
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		// wantCheckBBalance := choice == 1
+
+		if choice == 1 {
+			fmt.Println("Your Balance is ", accountBalance)
+		} else if choice == 2 {
+			fmt.Print("How much do you want to deposit? ")
+			fmt.Scan(&depostAmount)
+			if depostAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than zero.")
+				return
+			}
+
+			accountBalance = accountBalance + depostAmount
+			fmt.Printf("Depost %.1f. New Amount is %.1f\n", depostAmount, accountBalance)
+		} else if choice == 3 {
+			fmt.Print("How much do you want to withdraw? ")
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than zero.")
+				return
+			}
+
+			if withdrawAmount <= accountBalance {
+				accountBalance -= withdrawAmount
+				fmt.Printf("Withdraw Amount is %.1f. New Amount is %.1f\n", withdrawAmount, accountBalance)
+			} else {
+				fmt.Println("Ooops! Your are poor.")
+			}
+		} else if choice == 4 {
+			fmt.Println("Goodbye!")
+		}
 	}
-	fmt.Println("Welcome to Go Bank.")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1 Check balance")
-	fmt.Println("2 Deposit money")
-	fmt.Println("3 Withdraw money")
-	fmt.Println("4 Exit")
-
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
-
-	// wantCheckBBalance := choice == 1
-
-	if choice == 1 {
-		fmt.Println("Your Balance is ", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("How much do you want to deposit? ")
-		fmt.Scan(&depostAmount)
-		if depostAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than zero.")
-			return
-		}
-
-		accountBalance = accountBalance + depostAmount
-		fmt.Printf("Depost %.1f. New Amount is %.1f\n", depostAmount, accountBalance)
-	} else if choice == 3 {
-		fmt.Print("How much do you want to withdraw? ")
-		fmt.Scan(&withdrawAmount)
-		if withdrawAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than zero.")
-			return
-		}
-
-		if withdrawAmount <= accountBalance {
-			accountBalance -= withdrawAmount
-			fmt.Printf("Withdraw Amount is %.1f. New Amount is %.1f\n", withdrawAmount, accountBalance)
-		} else {
-			fmt.Println("Ooops! Your are poor.")
-		}
-	} else if choice == 4 {
-		fmt.Println("Goodbye!")
-	}
-
 }
