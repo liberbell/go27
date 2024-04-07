8834164b8c479f74a20b8d81b8082c6d09685fbb
Liberbell <liberbell@gmail.com>
2024-04-07 17:28:06 +0900

    bank1

--- lesson3/bank.go
+++ lesson3/bank.go
@@ -40,13 +40,8 @@ func main() {
 	var withdrawAmount float64
 
 	fmt.Println("Welcome to Go Bank.")
-	fmt.Println("What do you want to do?")
 
 	for {
-		fmt.Println("1 Check balance")
-		fmt.Println("2 Deposit money")
-		fmt.Println("3 Withdraw money")
-		fmt.Println("4 Exit")
 
 		var choice int
 		fmt.Print("Your choice: ")
@@ -124,4 +119,12 @@ func main() {
 		// }
 	}
 	fmt.Println("Thanks for choosing our bank.")
+}
+
+func presentOptions() {
+	fmt.Println("1 Check balance")
+	fmt.Println("2 Deposit money")
+	fmt.Println("3 Withdraw money")
+	fmt.Println("4 Exit")
+	fmt.Println("What do you want to do?")
 }
