5a79ff77431c23ff3143a4e5bec2f8bfa5ff84b6
Liberbell <liberbell@gmail.com>
2024-04-07 17:30:42 +0900

    communication1

--- lesson3/bank.go
+++ lesson3/bank.go
@@ -121,11 +121,3 @@ func main() {
 	}
 	fmt.Println("Thanks for choosing our bank.")
 }
-
-func presentOptions() {
-	fmt.Println("1 Check balance")
-	fmt.Println("2 Deposit money")
-	fmt.Println("3 Withdraw money")
-	fmt.Println("4 Exit")
-	fmt.Println("What do you want to do?")
-}
