f83bcb574a46cf21845ad0b90e5a244a7f3b9f13
Liberbell <liberbell@gmail.com>
2024-04-07 17:43:53 +0900

    bank8

--- lesson3/bank.go
+++ lesson3/bank.go
@@ -1,33 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 const accountBalanceFile = "balance.txt"
-
-func getFloatFromFile(fileName string) (float64, error) {
-	data, err := os.ReadFile(fileName)
-	if err != nil {
-		fmt.Println("Error reading balance file: ", err)
-		return 1000, errors.New("Failed to find file.")
-	}
-	valueText := string(data)
-	balance, err := strconv.ParseFloat(valueText, 64)
-	if err != nil {
-		fmt.Println("Error parsing balance: ", err)
-		return 1000, errors.New("Failed to parse stored value")
-	}
-	return balance, nil
-}
-
-func writeFloatTofile(value float64, fileName string) {
-	valueText := fmt.Sprint(value)
-	os.WriteFile(fileName, []byte(valueText), 0644)
-}
 
 func main() {
 	var accountBalance, err = getFloatFromFile(accountBalanceFile)
