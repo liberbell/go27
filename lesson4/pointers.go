6464bce7bcacc88dad3ce861f8379128ea277fe2
Liberbell <liberbell@gmail.com>
2024-04-09 08:40:38 +0900

    pointers9

--- lesson4/pointers.go
+++ lesson4/pointers.go
@@ -10,10 +10,11 @@ func main() {
 
 	fmt.Println("Age: ", agePointer)
 	fmt.Println("Age: ", *agePointer)
-	adultYears := getAdultYears(agePointer)
-	fmt.Println(adultYears)
+	getAdultYears(agePointer)
+	fmt.Println(age)
 }
 
 func getAdultYears(age *int) int {
-	return *age - 18
+	// return *age - 18
+	*age = *age - 8
 }
