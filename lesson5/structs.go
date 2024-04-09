c16612f82104036eafaa8ce5aa87bea0781e7d46
Liberbell <liberbell@gmail.com>
2024-04-10 08:35:44 +0900

    structs7

--- lesson5/structs.go
+++ lesson5/structs.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthDate := getUserData("Please enter your birthday: ")
+
+	fmt.Println(firstName, lastName, birthDate)
+}
+
+func outputUserDetail(firstName, lastName, birthDate string) {
 
 	fmt.Println(firstName, lastName, birthDate)
 }
