adb071e3689afc1c3cdac24fa64154fda386f9a2
Liberbell <liberbell@gmail.com>
2024-04-10 08:21:27 +0900

    structs3

--- lesson5/structs.go
+++ lesson5/structs.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
+	birthDate := getUserData("Please enter your birthday: ")
+
+	fmt.Println(firstName, lastName, birthDate)
 }
 
 func getUserData(promptoText string) string {
