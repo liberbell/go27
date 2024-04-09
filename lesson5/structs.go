48ab6fa45da38d70c9b10da5182b85d89cea2ec6
Liberbell <liberbell@gmail.com>
2024-04-10 08:20:21 +0900

    structs3

--- lesson5/structs.go
+++ lesson5/structs.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+}
+
+func getUserData(promptoText string) string {
+	fmt.Println(promptoText)
+	var value string
+	fmt.Scan(&value)
+	return value
 }
