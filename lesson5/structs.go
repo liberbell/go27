6062cdf6af8daf29c3019786d19e1f720770b9b5
Liberbell <liberbell@gmail.com>
2024-04-12 08:12:15 +0900

    structs2

--- lesson5/structs.go
+++ lesson5/structs.go
@@ -1,39 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
-
-func (u user) outputUserDetail() {
-
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthdate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("First name and Last Name and Birth date must be required")
-	}
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userfirstName := getUserData("Please enter your first name: ")
