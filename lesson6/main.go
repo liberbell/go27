ceb40996dd8710a4b0c58549dada1e70255aa0ac
Liberbell <liberbell@gmail.com>
2024-04-14 11:31:11 +0900

    main2

--- lesson6/main.go
+++ lesson6/main.go
@@ -1,8 +1,14 @@
 package main
 
+import "fmt"
+
 type customString string
 
+func (text customString) log() {
+	fmt.Println(text)
+}
+
 func main() {
-	var name customString
+	var name customString = "max"
 
 }
