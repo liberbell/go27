fac5d9c63f88642756a9fa83de1e7aa555c3ee20
Liberbell <liberbell@gmail.com>
2024-04-17 14:51:41 +0900

    todo1

---
+++ lesson6/todo/todo.go
@@ -0,0 +1,42 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Todo struct {
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func (note Note) Display() {
+	fmt.Printf("Your note titled %v has the following content: \n\n%v\n\n", note.Title, note.Content)
+}
+
+func (note Note) Save() error {
+	filename := strings.ReplaceAll(note.Title, " ", "_")
+	filename = strings.ToLower(filename) + ".json"
+	json, err := json.Marshal(note)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, json, 0644)
+}
+
+func New(title, content string) (Note, error) {
+	if title == "" {
+		return Note{}, errors.New("Invalid input.")
+	}
+
+	return Note{
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
+	}, nil
+}
