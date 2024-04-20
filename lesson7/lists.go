50e9f4dda23d80b6be6973af1edeaceb81543685
Liberbell <liberbell@gmail.com>
2024-04-20 18:28:33 +0900

    lists4

--- lesson7/lists.go
+++ lesson7/lists.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Product struct {
 	title string
 	id    string
@@ -8,4 +10,5 @@ type Product struct {
 
 func main() {
 	prices := [4]float64{10.99, 1.22, 33.4, 45.2}
+	fmt.Println(prices)
 }
