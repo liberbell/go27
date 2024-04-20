71d6d9962e809f7ccc03919846f713e354b9aa14
Liberbell <liberbell@gmail.com>
2024-04-20 18:33:10 +0900

    lists6

--- lesson7/lists.go
+++ lesson7/lists.go
@@ -10,7 +10,10 @@ type Product struct {
 
 func main() {
 	var productNames [4]string
+	productNames = [4]string{"A book"}
 	prices := [4]float64{10.99, 1.22, 33.4, 45.2}
 	fmt.Println(prices)
 	fmt.Println(productNames)
+
+	fmt.Println(prices[3])
 }
