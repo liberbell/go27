c0b579d05e67bd4a4f26ba7e557fd556c17139bf
Liberbell <liberbell@gmail.com>
2024-04-21 15:12:14 +0900

    practice2

--- prac2/practice.go
+++ prac2/practice.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	hobies := []string{"baseball", "movies", "books"}
+	fmt.Println(hobies)
+	fmt.Println(hobies[0])
+	newhobbies := hobies[1:2]
+	fmt.Println(newhobbies)
 }
