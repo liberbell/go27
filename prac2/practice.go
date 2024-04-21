6b155540dd66af9ea95aa6bb671647d9347d257d
Liberbell <liberbell@gmail.com>
2024-04-21 15:18:44 +0900

    practice4

--- prac2/practice.go
+++ prac2/practice.go
@@ -6,6 +6,8 @@ func main() {
 	hobies := []string{"baseball", "movies", "books"}
 	fmt.Println(hobies)
 	fmt.Println(hobies[0])
-	newhobbies := hobies[1:]
+	newhobbies := hobies[:2]
 	fmt.Println(newhobbies)
+
+	fmt.Println(cap(newhobbies))
 }
