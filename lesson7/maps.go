ecc4b075d42b91b224a37d534189bf36f84b1687
Liberbell <liberbell@gmail.com>
2024-04-23 08:29:01 +0900

    maps4

--- lesson7/maps.go
+++ lesson7/maps.go
@@ -9,4 +9,10 @@ func main() {
 	}
 	fmt.Println(website)
 	fmt.Println(website["Amazon Web Service"])
+
+	website["Linkedin"] = "https://www.linkedin.com"
+	fmt.Println(website)
+
+	delete(website, "Google")
+	fmt.Println(website)
 }
