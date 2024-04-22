2d5bb796d07a564b4c2092e1e9ad2bff1677e2b1
Liberbell <liberbell@gmail.com>
2024-04-23 08:35:24 +0900

    main2

--- lesson7/main.go
+++ lesson7/main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	userName := []string{}
+	userName := make([]string, 2)
 
 	userName = append(userName, "Max")
+	userName = append(userName, "Sam")
 	fmt.Println(userName)
 }
