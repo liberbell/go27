fb0bb422c8300d12610f800b3e842cdd0ee64206
Liberbell <liberbell@gmail.com>
2024-04-23 08:43:21 +0900

    main5

--- lesson7/main.go
+++ lesson7/main.go
@@ -15,8 +15,10 @@ func main() {
 	userName = append(userName, "Sam")
 	fmt.Println(userName)
 
-	courcesRaitings := map[string]float64{}
+	courcesRaitings := make(map[string]float64, 3)
 	courcesRaitings["go"] = 4.7
 	courcesRaitings["react"] = 4.8
+	courcesRaitings["angular"] = 4.5
+	courcesRaitings["node"] = 4.9
 	fmt.Println(courcesRaitings)
 }
