6962b7f16c8d245e490064ea81d7fb8b26389419
Liberbell <liberbell@gmail.com>
2024-04-24 08:31:07 +0900

    main2

--- lesson8/main.go
+++ lesson8/main.go
@@ -4,6 +4,7 @@ func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 }
 
-func dubleNumbers(number int) int {
-
+func dubleNumbers(number *[]int) int {
+	dNumbers := []int{}
+	for index, vl := range *number
 }
