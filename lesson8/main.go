ac7e16a6d37e630de1f1d67918fd1766b3013ab8
Liberbell <liberbell@gmail.com>
2024-04-24 08:47:10 +0900

    main10

--- lesson8/main.go
+++ lesson8/main.go
@@ -19,6 +19,14 @@ func dubleNumbers(numbers *[]int) []int {
 	return dNumbers
 }
 
+func transformNumbers(numbers *[]int, transform func(int)int) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, val*2)
+	}
+	return dNumbers
+}
+
 func double(number int) int {
 	return number * 2
 }
