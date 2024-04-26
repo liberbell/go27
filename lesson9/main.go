96582ddb4376dcf91c3a31d50b7cdf12f9a13529
Liberbell <liberbell@gmail.com>
2024-04-26 09:11:13 +0900

    main3

--- lesson9/main.go
+++ lesson9/main.go
@@ -1,9 +1,18 @@
 package main
 
+import "fmt"
+
+func main() {
+	fact := factorial(5)
+	fmt.Println(fact)
+}
+
 func factorial(number int) int {
 	result := 1
 
 	for i := 1; i <= number; i++ {
 		result = result * i
 	}
+
+	return result
 }
