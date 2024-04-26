86e567efe3dc576b7c1aebd1339280a9284d0792
Liberbell <liberbell@gmail.com>
2024-04-26 09:15:01 +0900

    main4

--- lesson9/main.go
+++ lesson9/main.go
@@ -8,11 +8,15 @@ func main() {
 }
 
 func factorial(number int) int {
-	result := 1
-
-	for i := 1; i <= number; i++ {
-		result = result * i
+	if number == 0 {
+		return 1
 	}
+	return number * factorial((number - 1))
+	// result := 1
 
-	return result
+	// for i := 1; i <= number; i++ {
+	// 	result = result * i
+	// }
+
+	// return result
 }
