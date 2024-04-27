2624680e1f3e23319c2f76fc935cbc23bf2a4b7f
Liberbell <liberbell@gmail.com>
2024-04-27 13:03:29 +0900

    main3

--- lesson10/main.go
+++ lesson10/main.go
@@ -2,14 +2,14 @@ package main
 
 func main() {
 	prices := []float64{10, 20, 30}
-	taxRates := []float64{0, 0.7, 0.1, 0.15}
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	result := make(map[float64][]float64)
 
-	for index, taxtaxRate := range taxRates {
-		var taxIncludedPrice []float64 = make([]float64, len(prices))
+	for index, taxRate := range taxRates {
+		var taxIncludedPrices []float64 = make([]float64, len(prices))
 		for priceIndex, price := range prices {
-
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
 		}
 	}
 }
