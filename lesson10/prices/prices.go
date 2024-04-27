0f6c1e882188bfc7a576aca889537e44895ebc65
Liberbell <liberbell@gmail.com>
2024-04-27 13:31:40 +0900

    prices5

--- lesson10/prices/prices.go
+++ lesson10/prices/prices.go
@@ -6,6 +6,16 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
+func (job TaxIncludedPriceJob) Process()  {
+	result := make(map[string]float64, 0)
+
+		for priceIndex, price := range job.InputPrices {
+			result[price] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+}
+
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
 		InputPrices: []float64{10, 20, 30},
