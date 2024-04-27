a84526c1eee6b964a83b8a0147bd09bf57524697
Liberbell <liberbell@gmail.com>
2024-04-27 13:10:04 +0900

    prices3

--- lesson10/prices/prices.go
+++ lesson10/prices/prices.go
@@ -1,6 +1,7 @@
 package prices
 
 type taxIncludedPriceJob struct {
-	TaxRate     float64
-	InputPrices []float64
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
 }
