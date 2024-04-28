8b4634c7c566fecc6aa7669e980413ad80ec6093
Liberbell <liberbell@gmail.com>
2024-04-28 11:02:00 +0900

    main4

--- lesson10/main.go
+++ lesson10/main.go
@@ -1,7 +1,8 @@
 package main
 
+import "example.com/price-calculator/prices"
+
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
