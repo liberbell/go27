964907fcc64778c7f4e1f4bd22b36b295519b2f9
Liberbell <liberbell@gmail.com>
2024-04-28 10:54:39 +0900

    main3

--- lesson10/main.go
+++ lesson10/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	_ "example.com/price-calculator/prices"
 )
 
@@ -10,10 +8,9 @@ func main() {
 	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
 		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-	fmt.Println(result)
+
 }
