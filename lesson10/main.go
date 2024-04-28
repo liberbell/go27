45cd7866481d1cac85bade406bd94affe57a7c69
Liberbell <liberbell@gmail.com>
2024-04-28 10:40:59 +0900

    main1

--- lesson10/main.go
+++ lesson10/main.go
@@ -9,11 +9,7 @@ func main() {
 	result := make(map[float64][]float64)
 
 	for _, taxRate := range taxRates {
-		var taxIncludedPrices []float64 = make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
 	}
 	fmt.Println(result)
 }
