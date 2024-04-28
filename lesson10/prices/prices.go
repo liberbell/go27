9034d92df8bbf4717d8f3f04a15c244ef79dcbc0
Liberbell <liberbell@gmail.com>
2024-04-28 10:56:10 +0900

    prices1

--- lesson10/prices/prices.go
+++ lesson10/prices/prices.go
@@ -14,6 +14,7 @@ func (job TaxIncludedPriceJob) Process() {
 	for _, price := range job.InputPrices {
 		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
 	}
+	fmt.Println(result)
 
 }
 
