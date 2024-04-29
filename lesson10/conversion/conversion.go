1df07de8238fbbc4c4d9a048d7440283c793523e
Liberbell <liberbell@gmail.com>
2024-04-29 09:58:25 +0900

    conversion4

--- lesson10/conversion/conversion.go
+++ lesson10/conversion/conversion.go
@@ -9,11 +9,12 @@ func StringToFloat(strings []string) ([]float64, error) {
 	var floats []float64
 
 	for stringIndex, stringVal := range strings {
-		floatPrice, err := strconv.ParseFloat(stringVal, 64)
+		floatVal, err := strconv.ParseFloat(stringVal, 64)
 		if err != nil {
 			return nil, errors.New("Failed to convert string to float")
 		}
+		floats = append(floats, floatVal)
 
 	}
-	job.InputPrices = prices
+	return floats, nil
 }
