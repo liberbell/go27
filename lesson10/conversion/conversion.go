6742a0b700ac11c290e64bac43f4310c58e1d086
Liberbell <liberbell@gmail.com>
2024-04-29 09:53:26 +0900

    conversion2

--- lesson10/conversion/conversion.go
+++ lesson10/conversion/conversion.go
@@ -1,5 +1,18 @@
 package conversion
 
-func StringToFloat() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func StringToFloat(strings []string) ([]float64, error) {
+	for stringIndex, string := range strings {
+		floatPrice, err := strconv.ParseFloat(string, 64)
+		if err != nil {
+			file.Close()
+			fmt.Println("Convert to float failed:", err)
+		}
+		prices[lineIndex] = floatPrice
+	}
+	job.InputPrices = prices
 }
