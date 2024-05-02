45a33524b4c75e4a6667a5243ff44b112782ed0c
Liberbell <liberbell@gmail.com>
2024-05-02 09:45:43 +0900

    main1

--- lesson10/main.go
+++ lesson10/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "example.com/price-calculator/prices"
+import (
+	"fmt"
+
+	"example.com/price-calculator/filemanager"
+	"example.com/price-calculator/prices"
+)
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
+		fm := filemanager.New("pricces.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
 		priceJob.Process()
 	}
