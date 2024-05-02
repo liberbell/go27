d4c1ef800b27add5ba3382bfedef07e9105d9152
Liberbell <liberbell@gmail.com>
2024-05-02 10:07:01 +0900

    cmdmanager3

--- lesson10/cmdmanager/cmdmanager.go
+++ lesson10/cmdmanager/cmdmanager.go
@@ -1,12 +1,24 @@
 package cmdmanager
 
+import "fmt"
+
 type CMDmanager struct {
 }
 
 func (cmd CMDmanager) ReadLines() ([]string, error) {
+	fmt.Println("Please enter your price. Confirm every price with Enter")
 
+	var prices []string
+
+	for {
+		var price string
+		fmt.Print("Price: ")
+		fmt.Scan()
+		prices = append(prices, price)
+	}
 }
 
 func (cmd CMDmanager) WriteResult(data interface{}) error {
-
+	fmt.Println(data)
+	return nil
 }
