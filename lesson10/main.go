0bf74f9b2c01b832d25d65e58612f8a65a097a97
Liberbell <liberbell@gmail.com>
2024-05-02 09:50:40 +0900

    main2

--- lesson10/main.go
+++ lesson10/main.go
@@ -11,8 +11,8 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("pricces.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		priceJob.Process()
 	}
 
