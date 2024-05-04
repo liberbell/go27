df381fc1b3bd269f64f7022212403b08d08c2b56
Liberbell <liberbell@gmail.com>
2024-05-04 10:05:45 +0900

    main7

--- lesson11/main.go
+++ lesson11/main.go
@@ -12,12 +12,14 @@ func greet(phrase string) {
 func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("Hello ", phrase)
+	doneChan <- true
 }
 
 func main() {
 	// greet("Nice to meet you")
 	// greet("How are you")
 	done := make(chan bool)
-	go slowGreet("How ... are ... you ...")
+	go slowGreet("How ... are ... you ...", done)
 	// greet("I home you`re liking the course")
+	<-done
 }
