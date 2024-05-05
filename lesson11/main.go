c5b9404e66aaf785b0e92f08e2ebccb4ea470ca1
Liberbell <liberbell@gmail.com>
2024-05-05 20:57:52 +0900

    main3

--- lesson11/main.go
+++ lesson11/main.go
@@ -18,9 +18,12 @@ func slowGreet(phrase string, doneChan chan bool) {
 
 func main() {
 	dones := make([]chan bool, 4)
-	done := make(chan bool)
-	go greet("Nice to meet you", done)
-	go greet("How are you", done)
+	// done := make(chan bool)
+
+	dones[0] = make(chan bool)
+	go greet("Nice to meet you", dones[0])
+	dones[1] = make(chan bool)
+	go greet("How are you", dones[1])
 	go slowGreet("How ... are ... you ...", done)
 	go greet("I home you`re liking the course", done)
 	<-done
