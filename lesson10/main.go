3ec80a5ccdb09fb24d7c04f843b42de178fc68fd
Liberbell <liberbell@gmail.com>
2024-05-05 21:21:21 +0900

    main3

--- lesson10/main.go
+++ lesson10/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
+	doneChans := make([]chan bool)
 
 	for _, taxRate := range taxRates {
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
