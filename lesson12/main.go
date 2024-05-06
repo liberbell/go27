d3bc04686ded9cc1eeff94b0bde75a67a0c484df
Liberbell <liberbell@gmail.com>
2024-05-07 08:50:41 +0900

    main3

--- lesson12/main.go
+++ lesson12/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-API/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvents)
+
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello"})
+	events := models.GetAllEvent()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvents(context *gin.Context) {
+
 }
