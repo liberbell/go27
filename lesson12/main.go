a62aee534d16a41d3105edb842cc3601bab609e4
Liberbell <liberbell@gmail.com>
2024-05-07 08:36:45 +0900

    main4

--- lesson12/main.go
+++ lesson12/main.go
@@ -4,5 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	server := gin.Default()
+	server.GET("/events", getEvents)
 	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+
 }
