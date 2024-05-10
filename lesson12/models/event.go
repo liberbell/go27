103800431e5b27f7ea50363d651d2679d1b44333
Liberbell <liberbell@gmail.com>
2024-05-10 09:41:53 +0900

    event6

--- lesson12/models/event.go
+++ lesson12/models/event.go
@@ -48,7 +48,7 @@ func GetAllEvent() ([]Event, error) {
 	var events []Event
 	for rows.Next() {
 		var event Event
-		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime)
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
 		if err != nil {
 			return nil, err
 		}
