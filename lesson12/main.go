8b7e83af1eaff89b2752d1ccc3535a3dad467144
Liberbell <liberbell@gmail.com>
2024-05-11 11:02:09 +0900

    main4

--- lesson12/main.go
+++ lesson12/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"example.com/rest-API/db"
+	"example.com/rest-API/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
 }
