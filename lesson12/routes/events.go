63ee5e52c0360a3a484b0663b2084e59d844a3cc
Liberbell <liberbell@gmail.com>
2024-05-11 11:06:31 +0900

    events6

--- lesson12/routes/events.go
+++ lesson12/routes/events.go
@@ -49,3 +49,16 @@ func createEvents(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+	event, err := models.GetEventByID(eventID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event.:2"})
+		return
+	}
+}
