057cdc37ba84dc988a2f8fdd5b197355bb103244
Liberbell <liberbell@gmail.com>
2024-05-14 08:31:44 +0900

    use2

--- lesson12/models/user.go
+++ lesson12/models/user.go
@@ -1,7 +1,17 @@
 package models
 
+import "example.com/rest-API/db"
+
 type User struct {
 	ID       int64
 	Email    string `binding: "required"`
 	Password string `binding: "required"`
+}
+
+func (u User) Save() error {
+	query := `INSERT INTO users(email, password) VALUES (?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
 }
