8bab08eccf959d44eca7590ce2741939c09d7ab6
Liberbell <liberbell@gmail.com>
2024-05-14 08:47:03 +0900

    users4

--- lesson12/routes/users.go
+++ lesson12/routes/users.go
@@ -15,4 +15,11 @@ func signup(context *gin.Context) {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Created user successfully."})
 }
