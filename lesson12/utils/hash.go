3bdd2e55657b9f85e259b5a9cc56fe79062c633e
Liberbell <liberbell@gmail.com>
2024-05-15 08:34:58 +0900

    hash1

--- lesson12/utils/hash.go
+++ lesson12/utils/hash.go
@@ -1,5 +1,10 @@
 package hash
 
-func HashPassword(password string) {
+import "golang.org/x/crypto/bcrypt"
 
+func HashPassword(password string) (string, error) {
+	hashed_passm, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return nil, err
+	}
 }
