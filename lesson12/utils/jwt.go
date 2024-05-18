5554db42b788d1a27a6b9ae2af29038f94e6417f
Liberbell <liberbell@gmail.com>
2024-05-18 10:12:54 +0900

    jwt3

--- lesson12/utils/jwt.go
+++ lesson12/utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -19,5 +20,11 @@ func GenerateToken(email string, userID int64) (string, error) {
 }
 
 func VerifyToken(token string) {
-	jwt.Parse(token, func(token jwt.Token) (interface{}, error) {})
+	jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Unexpected signing method.")
+		}
+		return secretKey, nil
+	})
 }
