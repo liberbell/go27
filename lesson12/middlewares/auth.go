61782b885a55a761c31e53ee94a86655b6e7ab45
Liberbell <liberbell@gmail.com>
2024-05-20 08:32:20 +0900

    auth2

--- lesson12/middlewares/auth.go
+++ lesson12/middlewares/auth.go
@@ -1,9 +1,22 @@
 package middlewares
 
 import (
+	"net/http"
+
+	"example.com/rest-API/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
 
+	userID, err := utils.VerifyToken(token)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
 }
