0ef26d9dd96c93ad41406cef0b1d3696e8c90109
Liberbell <liberbell@gmail.com>
2024-05-20 08:35:18 +0900

    auth2

--- lesson12/middlewares/auth.go
+++ lesson12/middlewares/auth.go
@@ -10,13 +10,15 @@ import (
 func Authenticate(context *gin.Context) {
 	token := context.Request.Header.Get("Authorization")
 	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
 		return
 	}
 
 	userID, err := utils.VerifyToken(token)
 	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
 		return
 	}
+
+	context.Next()
 }
