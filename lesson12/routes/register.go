6afd6740b7e628491dd5dc0e5d5e70da1f0a653f
Liberbell <liberbell@gmail.com>
2024-05-22 08:17:53 +0900

    register2

--- lesson12/routes/register.go
+++ lesson12/routes/register.go
@@ -1,7 +1,14 @@
 package routes
 
-func registerForEvents() {
+import (
+	"strconv"
 
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvents(context *gin.Context) {
+	userID := context.GetInt64("userID")
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
 }
 
 func cancelRegistration() {
